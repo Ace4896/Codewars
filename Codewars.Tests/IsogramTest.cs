94bbf8cb819b77b3b570e297b3502e43fcbbc258
Jon Pacheco <jon.pacheco@tutanota.com>
2022-01-31 13:24:32 +0000

    feat: add solution to isogram

---
+++ Codewars.Tests/IsogramTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class IsogramTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Dermatoglyphics").Returns(true);
+                yield return new TestCaseData("isogram").Returns(true);
+                yield return new TestCaseData("moose").Returns(false);
+                yield return new TestCaseData("isIsogram").Returns(false);
+                yield return new TestCaseData("aba").Returns(false);
+                yield return new TestCaseData("moOse").Returns(false);
+                yield return new TestCaseData("thumbscrewjapingly").Returns(true);
+                yield return new TestCaseData("").Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => Kata.IsIsogram(str);
+    }
+}
---
+++ Codewars/Isogram.cs
@@ -0,0 +1,32 @@
+﻿// Isograms are words that contain no repeating letters, consecutive or non-consecutive.
+// Goal for this one is to determine whether a string is an isogram.
+// Other notes:
+// - String only contains letters
+// - Empty string is an isogram
+// - Ignore letter case
+
+namespace Codewars
+{
+    using System.Collections.Generic;
+
+    public static partial class Kata
+    {
+        public static bool IsIsogram(string str)
+        {
+            var usedLetters = new HashSet<char>(26);
+
+            foreach (char c in str)
+            {
+                var lowerCase = char.ToLowerInvariant(c);
+                if (usedLetters.Contains(lowerCase))
+                {
+                    return false;
+                }
+
+                usedLetters.Add(lowerCase);
+            }
+
+            return true;
+        }
+    }
+}
