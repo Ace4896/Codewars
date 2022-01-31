944f2b3c1462662f28120aae54a64e989b2e4739
Jon Pacheco <jon.pacheco@tutanota.com>
2022-01-31 13:48:59 +0000

    feat: add solution to abbrevname

---
+++ Codewars/AbbrevName.cs
@@ -0,0 +1,13 @@
+﻿// Goal of this one is to convert a name into initials.
+// Always takes in a string with two names.
+
+namespace Codewars
+{
+    using System.Linq;
+
+    public static partial class Kata
+    {
+        public static string AbbrevName(string name) =>
+            string.Join('.', name.Split(' ').Select(n => char.ToUpperInvariant(n.First())));
+    }
+}
