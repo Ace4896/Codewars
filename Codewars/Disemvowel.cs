bb32514b380eb012c76d1e603a134287a4f226ab
Jon Pacheco <jon.pacheco@tutanota.com>
2022-01-31 13:17:37 +0000

    feat: add solution to disemvowel

---
+++ Codewars/Disemvowel.cs
@@ -0,0 +1,15 @@
+﻿// Goal of this one is to remove all vowels in the provided string.
+
+namespace Codewars
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class Kata
+    {
+        private static readonly HashSet<char> VOWELS = new HashSet<char>{ 'a', 'e', 'i', 'o', 'u' };
+        private static bool IsVowel(char c) => VOWELS.Contains(char.ToLowerInvariant(c));
+
+        public static string Disemvowel(string str) => string.Join("", str.Where(c => !IsVowel(c)));
+    }
+}
