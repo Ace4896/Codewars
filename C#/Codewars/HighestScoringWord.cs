73cb2b95982de10ff088fc4e8435f5644ed84fae
Jon Pacheco <jon.pacheco@tutanota.com>
2022-02-17 17:43:24 +0000

    feat: add c# solution to highest scoring word

---
+++ C#/Codewars/HighestScoringWord.cs
@@ -0,0 +1,28 @@
+﻿// Goal of this one is to score each word in a string based on their alphabet position (a = 1, b = 2 etc.)
+// Then return the word with the highest score
+
+namespace Codewars
+{
+    using System.Linq;
+
+    public static partial class Kata
+    {
+        public static string High(string s)
+        {
+            var max = 0;
+            var maxWord = "";
+
+            foreach (var word in s.Split(' '))
+            {
+                var score = word.Select(c => c - 'a' + 1).Sum();
+                if (score > max)
+                {
+                    max = score;
+                    maxWord = word;
+                }
+            }
+
+            return maxWord;
+        }
+    }
+}
