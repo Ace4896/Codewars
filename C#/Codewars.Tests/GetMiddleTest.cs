d75641e8ce56a934ce4abd194ea1422d50eff4dc
Jon Pacheco <jon.pacheco@tutanota.com>
2022-02-17 17:30:58 +0000

    feat: add c# solution for get middle

---
+++ C#/Codewars.Tests/GetMiddleTest.cs
@@ -0,0 +1,17 @@
+namespace Codewars.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GetMiddleTest
+    {
+        [Test]
+        public void GenericTests()
+        {
+            Assert.AreEqual("es", Kata.GetMiddle("test"));
+            Assert.AreEqual("t", Kata.GetMiddle("testing"));
+            Assert.AreEqual("dd", Kata.GetMiddle("middle"));
+            Assert.AreEqual("A", Kata.GetMiddle("A"));
+        }
+    }
+}
---
+++ C#/Codewars/GetMiddle.cs
@@ -0,0 +1,15 @@
+﻿// Goal of this one is to get the middle character in a string (or middle two characters for even length strings).
+
+namespace Codewars
+{
+    public static partial class Kata
+    {
+        public static string GetMiddle(string s)
+        {
+            var mid = s.Length / 2;
+            return s.Length % 2 == 0
+                ? s.Substring(mid - 1, 2)
+                : s.Substring(mid, 1);
+        }
+    }
+}
