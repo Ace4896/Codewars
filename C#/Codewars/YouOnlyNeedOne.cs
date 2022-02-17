ac93876e9fe5cb257a6b60086d9285cde92ab47a
Jon Pacheco <jon.pacheco@tutanota.com>
2022-02-17 17:33:30 +0000

    feat: add c# solution to you only need one

---
+++ C#/Codewars/YouOnlyNeedOne.cs
@@ -0,0 +1,11 @@
+﻿// Goal of this one is to check if an array contains the given value.
+
+namespace Codewars
+{
+    using System.Linq;
+
+    public static partial class Kata
+    {
+        public static bool Check(object[] a, object x) => a.Contains(x);
+    }
+}
