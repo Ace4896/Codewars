ead50421da43d2f860777cd6972cadba24d1892c
Jon Pacheco <jon.pacheco@tutanota.com>
2022-02-26 18:32:46 +0000

    feat: add c# solution to bit counting

---
+++ C#/Codewars/BitCounting.cs
@@ -0,0 +1,23 @@
+﻿// Goal of this one is to count the number of bits that are equal to 1 for the provided integer.
+
+namespace Codewars
+{
+    public static partial class Kata
+    {
+        public static int CountBits(int n)
+        {
+            int total = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                {
+                    total++;
+                }
+
+                n >>= 1;
+            }
+
+            return total;
+        }
+    }
+}
