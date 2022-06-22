6f6a60f52ce2d6f4345bf07b399b8edaa149d1c6
Jon Pacheco <jon.pacheco@tutanota.com>
2022-06-22 16:23:33 +0100

    feat: add c# solution to sort by levels

---
+++ C#/Codewars.Tests/SortByLevelsTest.cs
@@ -0,0 +1,24 @@
+namespace Codewars.Tests
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SortByLevelsTest
+    {
+        [Test]
+        public void NullTest()
+        {
+            Assert.AreEqual(new List<int>(), Kata.TreeByLevels(null));
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(
+                new List<int>() { 1, 2, 3, 4, 5, 6 },
+                Kata.TreeByLevels(new Node(new Node(null, new Node(null, null, 4), 2), new Node(new Node(null, null, 5), new Node(null, null, 6), 3), 1))
+            );
+        }
+    }
+}
---
+++ C#/Codewars/SortByLevels.cs
@@ -0,0 +1,52 @@
+﻿namespace Codewars
+{
+    using System.Collections.Generic;
+
+    public static partial class Kata
+    {
+        public static List<int> TreeByLevels(Node? node)
+        {
+            if (node == null)
+            {
+                return new List<int>();
+            }
+
+            var nodeQueue = new Queue<Node>();
+            var sorted = new List<int>();
+
+            nodeQueue.Enqueue(node);
+            while (nodeQueue.TryDequeue(out var nextNode))
+            {
+                // Add this node's value to the list
+                sorted.Add(nextNode.Value);
+
+                // Then add left child + right child to be processed later
+                if (nextNode.Left != null)
+                {
+                    nodeQueue.Enqueue(nextNode.Left);
+                }
+
+                if (nextNode.Right != null)
+                {
+                    nodeQueue.Enqueue(nextNode.Right);
+                }
+            }
+
+            return sorted;
+        }
+    }
+
+    public class Node
+    {
+        public Node? Left;
+        public Node? Right;
+        public int Value;
+
+        public Node(Node? l, Node? r, int v)
+        {
+            Left = l;
+            Right = r;
+            Value = v;
+        }
+    }
+}
