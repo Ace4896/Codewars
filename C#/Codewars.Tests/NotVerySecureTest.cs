748a71680a85e364c2629f39621e4d060fac0706
Jon Pacheco <jon.pacheco@tutanota.com>
2022-06-23 10:23:48 +0100

    feat: add c# solution to "not very secure"

---
+++ C#/Codewars.Tests/NotVerySecureTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Codewars.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class NotVerySecureTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Mazinkaiser").Returns(true);
+                yield return new TestCaseData("hello world_").Returns(false);
+                yield return new TestCaseData("PassW0rd").Returns(true);
+                yield return new TestCaseData("     ").Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => Kata.Alphanumeric(str);
+    }
+}
---
+++ C#/Codewars/NotVerySecure.cs
@@ -0,0 +1,10 @@
+﻿namespace Codewars
+{
+    using System.Linq;
+
+    public static partial class Kata
+    {
+        // Not sure why this counts as 5 kyu, but oh well...
+        public static bool Alphanumeric(string str) => !string.IsNullOrEmpty(str) && str.All(char.IsLetterOrDigit);
+    }
+}
