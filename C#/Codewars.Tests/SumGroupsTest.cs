a9536f7cd3f45cb9abb60a4fb7b288956ff87d0a
Jon Pacheco <jon.pacheco@tutanota.com>
2022-07-04 15:09:57 +0100

    feat: add c# solution to "Sum Groups"

---
+++ C#/Codewars.Tests/SumGroupsTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Codewars.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SumGroupsTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(6, Kata.SumGroups(new int[] { 2, 1, 2, 2, 6, 5, 0, 2, 0, 5, 5, 7, 7, 4, 3, 3, 9 }));
+            Assert.AreEqual(5, Kata.SumGroups(new int[] { 2, 1, 2, 2, 6, 5, 0, 2, 0, 3, 3, 3, 9, 2 }));
+            Assert.AreEqual(1, Kata.SumGroups(new int[] { 1 }));
+            Assert.AreEqual(1, Kata.SumGroups(new int[] { 2 }));
+            Assert.AreEqual(2, Kata.SumGroups(new int[] { 1, 2 }));
+            Assert.AreEqual(1, Kata.SumGroups(new int[] { 1, 1, 2, 2 }));
+        }
+    }
+}
---
+++ C#/Codewars/SumGroups.cs
@@ -0,0 +1,53 @@
+﻿namespace Codewars
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static partial class Kata
+    {
+        // NOTE: A recursive solution is shorter and more readable
+        public static int SumGroups(int[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+
+            List<int> finalList = new(arr);
+            List<int> tmpList = new();
+            bool sumPerformed;
+
+            do
+            {
+                bool isEven = finalList[0] % 2 == 0;
+                int currentSum = finalList[0];
+                sumPerformed = false;
+
+                foreach (int num in finalList.Skip(1))
+                {
+                    if (num % 2 == 0 == isEven)
+                    {
+                        currentSum += num;
+                        sumPerformed = true;
+                    }
+                    else
+                    {
+                        tmpList.Add(currentSum);
+                        currentSum = num;
+                        isEven = !isEven;
+                    }
+                }
+
+                tmpList.Add(currentSum);
+
+                finalList.Clear();
+                finalList.AddRange(tmpList);
+
+                tmpList.Clear();
+            }
+            while (sumPerformed);
+
+            return finalList.Count;
+        }
+    }
+}
