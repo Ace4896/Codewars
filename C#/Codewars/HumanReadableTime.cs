78e153a0a2088552a2f368aed80fa8ecb6f66e22
Jon Pacheco <jon.pacheco@tutanota.com>
2022-07-08 08:47:08 +0100

    feat: add c# solution to human readable time

---
+++ C#/Codewars/HumanReadableTime.cs
@@ -0,0 +1,19 @@
+﻿namespace Codewars
+{
+    public static class TimeFormat
+    {
+        // NOTE: Other option is to perform the math directly from the number of seconds
+        public static string GetReadableTime(int seconds)
+        {
+            int tmp = seconds;
+
+            int hours = seconds / 3600;
+            tmp -= hours * 3600;
+
+            int minutes = tmp / 60;
+            tmp -= minutes * 60;
+
+            return $"{hours:D2}:{minutes:D2}:{tmp:D2}";
+        }
+    }
+}
