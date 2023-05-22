7ebc1139bf519521053f6ac468377010d69d9fe8
Jon Pacheco <jon.pacheco@tutanota.com>
2023-05-22 10:25:39 +0100

    feat: add VB solution for century from year

---
+++ VB/Codewars.VB/CenturyFromYear.vb
@@ -0,0 +1,16 @@
+﻿Namespace Codewars.VB
+    Partial Public Module Kata
+        Public Function Century(ByVal year As Integer) As Integer
+            Dim result As Integer
+            Dim remainder As Integer
+
+            result = Math.DivRem(year, 100, remainder)
+
+            If remainder > 0 Then
+                result += 1
+            End If
+
+            Return result
+        End Function
+    End Module
+End Namespace
