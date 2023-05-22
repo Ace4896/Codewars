9bf3dfcfa15a9d6c685342d28872fff9f2a3324b
Jon Pacheco <jon.pacheco@tutanota.com>
2023-05-22 10:52:43 +0100

    feat: add VB solution to easy line
    not a great solution... there was an easier option which involved just
    implementing the 'choose' function.

---
+++ VB/Codewars.VB/EasyLine.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Numerics
+
+Namespace Codewars.VB
+    Public Module Easyline
+        Public Function EasyLine(ByVal n As Integer) As BigInteger
+            ' Binomial Coefficient (n, k) = n! / (k! * (n - k)!)
+            Dim result As BigInteger = 0
+            Dim nFactorial As BigInteger = Factorial(n)
+
+            For k As Integer = 0 To n
+                Dim kFactorial As BigInteger = Factorial(k)
+                Dim nMinusKFactorial As BigInteger = Factorial(n - k)
+                Dim value As BigInteger = nFactorial / (kFactorial * nMinusKFactorial)
+
+                result += BigInteger.Pow(value, 2)
+            Next
+
+            Return result
+        End Function
+
+        Function Factorial(ByVal n As Integer) As BigInteger
+            Dim result As BigInteger = 1
+
+            For i As BigInteger = 2 To n
+                result *= i
+            Next
+
+            Return result
+        End Function
+    End Module
+End Namespace
---
+++ VB/Codewars.VB/EasyLineTests.vb
@@ -0,0 +1,20 @@
+Imports NUnit.Framework
+
+Namespace Codewars.VB
+    <TestFixture>
+    Public Class EasyLineTests
+        Sub testing(ByVal n As Integer, ByVal sexp As String)
+            Dim sactual As String = Easyline.EasyLine(n).ToString()
+            Assert.AreEqual(sexp, sactual)
+        End Sub
+
+        <Test>
+        Sub BasicTests()
+            testing(7, "3432")
+            testing(13, "10400600")
+            testing(17, "2333606220")
+            testing(19, "35345263800")
+
+        End Sub
+    End Class
+End Namespace
