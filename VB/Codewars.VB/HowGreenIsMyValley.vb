6d347a12e6402e4d859ce28b9374f48aa165d4bc
Jon Pacheco <jon.pacheco@tutanota.com>
2023-05-23 10:56:05 +0100

    feat: add VB solution to how green is my valley

---
+++ VB/Codewars.VB/HowGreenIsMyValley.vb
@@ -0,0 +1,30 @@
+﻿Namespace Codewars.VB
+    Public Module Valley
+        Public Function MakeValley(ByVal arr As Integer()) As Integer()
+            If arr.Length = 0 Then
+                Return Array.Empty(Of Integer)
+            End If
+
+            ' Sort the array in descending order
+            ' Add each number to the final array, alternating between left and right positions
+            Dim valley As Integer() = New Integer(arr.Length - 1) {}
+            Dim left As Integer = 0
+            Dim right As Integer = arr.Length - 1
+            Dim fillLeft As Boolean = True
+
+            For Each num In arr.OrderByDescending(Function(i) i)
+                If fillLeft Then
+                    valley(left) = num
+                    left += 1
+                Else
+                    valley(right) = num
+                    right -= 1
+                End If
+
+                fillLeft = Not fillLeft
+            Next
+
+            Return valley
+        End Function
+    End Module
+End Namespace
---
+++ VB/Codewars.VB/HowGreenIsMyValleyTests.vb
@@ -0,0 +1,27 @@
+Imports NUnit.Framework
+
+Namespace Codewars.VB
+    <TestFixture>
+    Public Class HowGreenIsMyValleyTests
+        Private Function Array2String(ByVal list As Integer()) As String
+            Return "[" & String.Join(", ", list) & "]"
+        End Function
+        Sub testing(ByVal actual As String, ByVal exp As String)
+            Assert.AreEqual(exp, actual)
+        End Sub
+
+        <Test>
+        Sub BasicTests()
+            Dim a As Integer() = New Integer() {17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1}
+            Dim r As Integer() = New Integer() {17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17}
+            testing(Array2String(Valley.MakeValley(a)), Array2String(r))
+            a = New Integer() {20, 7, 6, 2}
+            r = New Integer() {20, 6, 2, 7}
+            testing(Array2String(Valley.MakeValley(a)), Array2String(r))
+            a = New Integer() {}
+            r = New Integer() {}
+            testing(Array2String(Valley.MakeValley(a)), Array2String(r))
+
+        End Sub
+    End Class
+End Namespace
