438e051f82aa798162d01dcbd1537b7fad18151e
Jon Pacheco <jon.pacheco@tutanota.com>
2023-05-24 11:17:26 +0100

    feat: add VB solution to Hungarian vowel harmony (harder)

---
+++ VB/Codewars.VB/VowelHarmonyHarder.vb
@@ -0,0 +1,74 @@
+﻿Namespace Codewars.VB
+    Partial Public Module Kata
+        Private ReadOnly FrontVowels As HashSet(Of Char) = New HashSet(Of Char) From {"e", "é", "i", "í", "ö", "ő", "ü", "ű"}
+        Private ReadOnly BackVowels As HashSet(Of Char) = New HashSet(Of Char) From {"a", "á", "o", "ó", "u", "ú"}
+        Private ReadOnly ShortToLong As Dictionary(Of Char, Char) = New Dictionary(Of Char, Char) From
+        {
+            {"a", "á"},
+            {"e", "é"},
+            {"i", "í"},
+            {"o", "ó"},
+            {"u", "ú"},
+            {"ö", "ő"},
+            {"ü", "ű"}
+        }
+
+        Private ReadOnly Digraphs As HashSet(Of String) = New HashSet(Of String) From {"sz", "zs", "cs"}
+
+        Public Function Instrumental(word As String) As String
+            Dim lastChar = word(word.Length - 1)
+            Dim result As String
+
+            If FrontVowels.Contains(lastChar) Or BackVowels.Contains(lastChar) Then
+                ' Ends in a vowel
+                result = word.Substring(0, word.Length - 1)
+
+                ' Convert ending vowel to long vowel if needed
+                Dim longVowel As Char
+                If ShortToLong.TryGetValue(lastChar, longVowel) Then
+                    result += longVowel
+                Else
+                    result += lastChar
+                End If
+
+                ' Append suffix
+                If FrontVowels.Contains(lastChar) Then
+                    result += "vel"
+                Else
+                    result += "val"
+                End If
+            Else
+                ' Ends in a consonant
+                ' Check for digraph first
+                Dim endingDigraph As String = Nothing
+
+                For Each digraph In Digraphs
+                    If word.EndsWith(digraph) Then
+                        endingDigraph = digraph
+                        Exit For
+                    End If
+                Next
+
+                If endingDigraph IsNot Nothing Then
+                    ' Remove original digraph and add new digraph with doubled first letter
+                    result = word.Substring(0, word.Length - endingDigraph.Length) + endingDigraph(0) + endingDigraph
+                Else
+                    ' Double ending consonant
+                    result = word + lastChar
+                End If
+
+                For Each c As Char In word.Reverse()
+                    If FrontVowels.Contains(c) Then
+                        result += "el"
+                        Exit For
+                    ElseIf BackVowels.Contains(c) Then
+                        result += "al"
+                        Exit For
+                    End If
+                Next
+            End If
+
+            Return result
+        End Function
+    End Module
+End Namespace
---
+++ VB/Codewars.VB/VowelHarmonyHarderTests.vb
@@ -0,0 +1,34 @@
+﻿Imports NUnit.Framework
+
+Namespace Codewars.VB
+    <TestFixture>
+    Public Class VowelHarmonyHarderTests
+        Private Shared Tests As String(,) = {
+             {"ablak", "ablakkal"},
+             {"betű", "betűvel"},
+             {"fa", "fával"},
+             {"gonosz", "gonosszal"},
+             {"kar", "karral"},
+             {"keret", "kerettel"},
+             {"kosz", "kosszal"},
+             {"otthon", "otthonnal"},
+             {"rokkant", "rokkanttal"},
+             {"rács", "ráccsal"},
+             {"szék", "székkel"},
+             {"teve", "tevével"},
+             {"tükör", "tükörrel"},
+             {"virág", "virággal"}
+        }
+
+        <Test>
+        Public Sub SampleTests()
+            For i As Integer = 0 To Tests.GetLength(0) - 1
+                Dim input = Tests(i, 0)
+                Dim actual = Kata.Instrumental(input)
+                Dim expected = Tests(i, 1)
+                Console.WriteLine("{0} -> {1}", input, expected)
+                Assert.AreEqual(expected, actual)
+            Next
+        End Sub
+    End Class
+End Namespace
