c7ef1013ae75781428a46c49db0e14bd321c17ce
Jon Pacheco <jon.pacheco@tutanota.com>
2023-05-24 10:46:36 +0100

    feat: add VB solution to parts of a list

---
+++ VB/Codewars.VB/PartsOfAList.vb
@@ -0,0 +1,25 @@
+﻿Namespace Codewars.VB
+    Public Module PartList
+        Public Function Partlist(ByVal arr As String()) As String()()
+            ' Base Case: Only 2 elements
+            ' Just return input array
+            If arr.Length = 2 Then
+                Return New String()() {arr}
+            End If
+
+            ' Main Case: 3+ elements
+            ' There are n-1 parts
+            Dim partsCount As Integer = arr.Length - 1
+            Dim parts = New String(partsCount - 1)() {}
+
+            For firstPartCount As Integer = 1 To partsCount
+                Dim firstPart As String = String.Join(" ", arr.Take(firstPartCount))
+                Dim secondPart As String = String.Join(" ", arr.Skip(firstPartCount))
+
+                parts(firstPartCount - 1) = New String() {firstPart, secondPart}
+            Next
+
+            Return parts
+        End Function
+    End Module
+End Namespace
---
+++ VB/Codewars.VB/PartsOfAListTests.vb
@@ -0,0 +1,31 @@
+Imports NUnit.Framework
+
+Namespace Codewars.VB
+    <TestFixture>
+    Public Class PartsOfAListTests
+        Function Array2DToString(ByVal hM As String()()) As String
+            Dim res As String = ""
+            For i As Integer = 0 To hM.Length - 1
+                res += "[" & String.Join(", ", hM(i)) & "], "
+            Next
+            Return "[" & res.Substring(0, res.Length - 2) & "]"
+        End Function
+
+        Sub testing(ByVal arr As String(), ByVal sexp As String)
+            Dim actual = PartList.Partlist(arr)
+            Dim sactual = Array2DToString(actual)
+            Assert.AreEqual(sexp, sactual)
+        End Sub
+
+        <Test>
+        Sub BasicTestsPartlist()
+            Dim s1 As String() = New String() {"cdIw", "tzIy", "xDu", "rThG"}
+            Dim a As String = "[[cdIw, tzIy xDu rThG], [cdIw tzIy, xDu rThG], [cdIw tzIy xDu, rThG]]"
+            testing(s1, a)
+
+            s1 = New String() {"I", "wish", "I", "hadn't", "come"}
+            a = "[[I, wish I hadn't come], [I wish, I hadn't come], [I wish I, hadn't come], [I wish I hadn't, come]]"
+            testing(s1, a)
+        End Sub
+    End Class
+End Namespace
